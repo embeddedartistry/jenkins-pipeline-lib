3c4a5bf573f8758be18a9cead25f8479eead8629
Phillip Johnston <phillip@embeddedartistry.com>
2017-09-01 16:10:20 -0700

    Make execute a functiong

--- vars/gitTagCleanup.groovy
+++ vars/gitTagCleanup.groovy
@@ -5,6 +5,6 @@ import hudson.scm.ChangeLogSet;
 * Delete the temporary build tag that we created
 */
 def call(String build) {
-  "git tag -d '${build}'".execute
+  "git tag -d '${build}'".execute()
 }
 
