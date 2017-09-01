3a7539a6e82273068039fd5e5d98acefeaec6819
Phillip Johnston <phillip@embeddedartistry.com>
2017-08-31 17:11:18 -0700

    Try truncating commit

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -79,7 +79,8 @@ def getChangeString(List<ChangeLogSet<? extends ChangeLogSet.Entry>> changeLogSe
     for (int j = 0; j < entries.length; j++) {
       def entry = entries[j]
       truncated_msg = entry.msg.take(MAX_MSG_LEN)
-      changeString += "`${entry.commitId}` ${truncated_msg} [${entry.author}]\n"
+      truncated_commit = entry.commitId.take(7)
+      changeString += "- `${truncated_commit}`: ${truncated_msg} [${entry.author}]\n"
     }
   }
 
