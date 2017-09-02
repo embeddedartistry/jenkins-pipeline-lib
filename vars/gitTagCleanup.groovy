f44fb1b26c4af0c55a3a4044d6075bfdade8db5d
Phillip Johnston <phillip@embeddedartistry.com>
2017-09-01 17:05:15 -0700

    Try using bin/bash method

--- vars/gitTagCleanup.groovy
+++ vars/gitTagCleanup.groovy
@@ -1,5 +1,4 @@
 #!/usr/bin/env groovy
-import hudson.scm.ChangeLogSet;
 
 /**
 * Delete the temporary build tag that we created
--- vars/gitTagPreBuild.groovy
+++ vars/gitTagPreBuild.groovy
@@ -7,15 +7,10 @@ import hudson.scm.ChangeLogSet;
 * By using an annotated tag, we can populate the build version details from git
 */
 def call(String build) {
-  StringBuffer sbout = new StringBuffer()
-  StringBuffer sberr = new StringBuffer()
   println 'Creating a temporary build tag for versioning'
-  println "git tag -a '${build}' -m 'Creating tag to build ${build}'"
-  def p = ["git", "tag", "-a", "'${build}'", "-m", "'Creating tag to build ${build}'"].execute()
-  p.waitForProcessOutput sbout, sberr
-  println "${sberr.toString()}"
-  println "Erorr lines: ${sberr.toString().readLines().size()}"
-  println "Exit value: ${p.exitValue()}"
-  println "Output: ${p.text}"
+  def command = "/bin/bash -c \"git tag -a '${build}' -m 'Creating tag to build ${build}'\""
+  println "${command}"
+  def p = command.execute()
+  p.waitFor()
 }
 
