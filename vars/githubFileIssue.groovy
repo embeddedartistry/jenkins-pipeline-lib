f61d4df6d5d6a317dd7ead11ba1bc1d0125c663e
Phillip Johnston <phillip@embeddedartistry.com>
2017-09-11 10:17:06 -0700

    GitHub File Issue Improvements

--- vars/githubFileIssue.groovy
+++ vars/githubFileIssue.groovy
@@ -1,18 +1,27 @@
 #!/usr/bin/env groovy
 
-def call(String title = null, String label = '')
+/**
+* At the time of this implementation, issueLabel appears to be unreliable.
+* Setting a value to anything other than '' causes the issue notification
+* step to fail. I am not inclined to debug this, and will simply expect the
+* User to rely on their own template on the Jenkins master configuration page.
+*/
+
+def call(String title = null, String status = 'failed')
 {
-  echo 'Build failed! Filing GitHub issue'
+  echo 'Filing GitHub issue with build status: $status'
 
   if(title == null)
   {
-    title = "${env.BUILD_DISPLAY_NAME} failed"
+    title = "${env.JOB_NAME}: #${env.BUILD_NUMBER} ${status}"
   }
 
   step([$class: 'GitHubIssueNotifier',
         issueAppend: true,
-        issueLabel: $label,
-        issueTitle: $title])
+        issueLabel: '',
+        issueTitle: "$title"])
+
+  echo 'Github issue published successfully'
 }
 
 /* TODO: allow overriding issue body
