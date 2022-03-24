b32310c8b803bf033c2f933df6cb8f1eadbcfb5e
Phillip Johnston <phillip@embeddedartistry.com>
2022-03-24 12:16:53 -0700

    WIP

--- vars/setNextBuildNumber.groovy
+++ vars/setNextBuildNumber.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
 
 @NonCPS
-def setNextBuildNumber(build_number) {
+def setNextBuildNumber(String build_number_string) {
+  Integer build_number = build_number_string as Integer
   def job = Jenkins.instance.getItemByFullName(env.JOB_NAME, Job.class)
   job.nextBuildNumber = build_number
   job.saveNextBuildNumber()
