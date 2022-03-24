5604001ef150008efb792a44e68fba38bcd53df8
Phillip Johnston <phillip@embeddedartistry.com>
2022-03-24 12:10:31 -0700

    Add a command for setting the Build Number

---
+++ vars/setNextBuildNumber.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+@NonCPS
+def call(String build_number_string) {
+  Integer build_number = build_number_string as Integer
+  def job = Jenkins.instance.getItemByFullName(env.JOB_NAME, Job.class)
+  job.nextBuildNumber = build_number
+  job.saveNextBuildNumber()
+  return true
+}
